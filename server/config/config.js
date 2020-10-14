fb6588e7062eb2537b91b16e96ba4031d41ad959
fernabe <fer.barrabes@gmail.com>
2020-10-14 16:05:17 +0200

    Variable de entorno Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://adirest:y91XVLtBeEwRtjn4@cluster0.nievr.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
